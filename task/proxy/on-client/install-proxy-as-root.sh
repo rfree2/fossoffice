978061a241816cb74f62b446dd5ced005bec14f6
rfree2 <rfree@op.pl>
2014-04-30 14:56:23 +0200

    [ok] proxy setup script should be fine now

--- task/proxy/on-client/install-proxy-as-root.sh
+++ task/proxy/on-client/install-proxy-as-root.sh
@@ -1,7 +1,8 @@
 ip_base=192.168.5
-echo "Using ip_base=$ip_base"
 
-http_proxy=http://$ip_base.8:3128
-https_proxy=http://$ip_base.8:3128
-no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,*.l,*.local,*.office,*.private,*.priv,*.lan
+envfile=/etc/environment 
+echo "Using ip_base=$ip_base and appending to end of $envfile (is there were old settings for proxy there, you might wish to clean it up later)"
 
+http_proxy=http://$ip_base.8:3128 >> $envfile
+https_proxy=http://$ip_base.8:3128 >> $envfile
+no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,*.l,*.local,*.office,*.private,*.priv,*.lan >> $envfile
