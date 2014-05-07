a4dc5908b7a6b92feb93e47f4488970b66d36e0d
rfree2 <rfree@op.pl>
2014-05-07 13:10:59 +0200

    test c++ file

---
+++ test/a/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello " << __FILE__ << endl;
+
+	string text = "asdf,gs,asfsafasdsa,sdf,asgsgsagsffsdffas";
+
+
+}
+
