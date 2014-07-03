6eb54be60792ec04b18266a128ab62386198ad55
Happuri <happuri@gmx.com>
2014-07-03 12:29:51 +0200

    [fix] Saving to envfile

--- task/proxy/on-client/install-proxy-as-root.sh
+++ task/proxy/on-client/install-proxy-as-root.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 ip_base=192.168.5
 
 envfile=/etc/environment 
 echo "Using ip_base=$ip_base and appending to end of $envfile (is there were old settings for proxy there, you might wish to clean it up later)"
 
-http_proxy=http://$ip_base.8:3128 >> $envfile
-https_proxy=http://$ip_base.8:3128 >> $envfile
-no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,*.l,*.local,*.office,*.private,*.priv,*.lan >> $envfile
+echo "http_proxy=http://$ip_base.8:3128" >> $envfile
+echo "https_proxy=http://$ip_base.8:3128" >> $envfile
+echo "no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,*.l,*.local,*.office,*.private,*.priv,*.lan" >> $envfile
+
