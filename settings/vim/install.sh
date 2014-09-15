c868fc2ae91680908d80a6d164dde6e2cca1d30c
rfree2 <rfree@op.pl>
2014-09-15 14:36:55 +0200

    vim install correct when reinstall (delete old)

--- settings/vim/install.sh
+++ settings/vim/install.sh
@@ -4,7 +4,7 @@ target='~/.vim'
 echo "Delete and overwrite directory $target (btw now HOME=$HOME) ? Type uppercase Y to continue, n or no to abort"
 read yn
 if [[ $yn != "Y" ]] ; then echo "Abort"; exit 1 ; fi
-cp -ar _.vim ~/.vim
+rm -rf ~/.vim  && cp -ar _.vim/ ~/.vim
 
 target='~/.vimrc'
 echo "Delete and overwrite $target (btw now HOME=$HOME) ? Type uppercase Y to continue, n or no to abort"
